bebb2777a6632f665219281ecb2f02308a2c47e5
Alok kothiyal <alokothiyal007@gmail.com>
2020-09-28 08:21:44 +0530

    added social profiles

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Styles from './HomePage.module.css'
 import { MdLocationOn,MdCall,MdEmail } from 'react-icons/md';
+import {Link} from 'react-router-dom'
 
 
 class HomePage extends React.Component{
@@ -14,6 +15,8 @@ class HomePage extends React.Component{
                     <div className={`position-absolute ${Styles.decorative_circle}`}></div>
                 </div>
 
+                
+
                 <div className={Styles.name}>
                     Alok Kothiyal
                 </div>
@@ -21,29 +24,58 @@ class HomePage extends React.Component{
                 <div className={Styles.profession}>
                     Full Stack Developer
                 </div>
+                <br></br>
+                <div className="row">
+                    <div className="col-3">
+
+                    </div>
+                    <div className="col-2">
+                        <Link to={{pathname:"https://www.linkedin.com/in/alok-kothiyal-025191119/"}} target="_blank">
+                            <img src="linkedin.png" alt="logo" width="40px"></img>
+                            <div>
+                               alok-kothiyal
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="col-2">
+                        <Link to={{pathname:"https://twitter.com/alok_kothiyal"}} target="_blank">
+                            <img src="twitter.png" alt="logo" width="40px"></img>
+                            <div>
+                            alok_kothiyal
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="col-2">
+                        <Link to={{pathname:"https://github.com/AlokKothiyal1"}} target="_blank">
+                            <img src="github.svg" alt="logo" width="40px"></img>
+                            <div>
+                                 alokKothiyal1
+                            </div>
+                        </Link>
+                    </div>
+                </div>
 
                 <div className={`row m-4 ${Styles.details}`}>
                     <div className="col">
                         <MdLocationOn size="40px" color="#FFC831"/>
-                        <br></br>
-                        Dehradun,India
+                       Dehradun,India
                     </div>
 
                     <div className="col">
                        <MdCall size="40px" color="#FFC831"/> 
-                       <br></br>
-                       +91 789 5108 898
+                      +91 789 5108 898
                     </div>
 
                     <div className="col">
                         <MdEmail size="40px" color="#FFC831"/> 
-                        <br></br>
-                        alokkothiyal08@gmail.com
+                       alokkothiyal08@gmail.com
                     </div>
                 </div>
 
                 <div className={`mb-5 px-5 ${Styles.details}`}>
-                    A passionate aspiring Full Stack Developer skilled in MERN stack,moulded and shaped by Masai School's numerous training.Actively ready to join in a great
+                    A passionate aspiring Full Stack Developer skilled in MERN stack.
+                    <br></br>
+                    Moulded and shaped by Masai School's numerous training. Actively ready to join in a great
                     lively team in a good start-up environment and give my best.
                 </div>
 
