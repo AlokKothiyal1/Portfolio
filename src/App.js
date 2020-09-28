51f42e362da8e67938b9f2b8793f5e983bf912cb
Alok kothiyal <alokothiyal007@gmail.com>
2020-09-28 10:28:43 +0530

    pushed whole code

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import HomePage from './components/HomePage'
 import Proficiencies from './components/Proficiencies'
 import Projects from './components/Projects'
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <HomePage/>
       <hr></hr>
       <Proficiencies/>
-      
       <Projects/>
     </div>
+    </BrowserRouter>
   );
 }
 
